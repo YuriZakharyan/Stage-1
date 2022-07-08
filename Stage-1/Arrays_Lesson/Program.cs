2daf8ec03085dcf6258c5acdc9bee941f5dc01f1
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-07-09 00:39:36 +0400

    Arrays, Sort array, Ternary operator, nested loops,

---
+++ Stage-1/Arrays_Lesson/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace Arrays_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Ternary operator
+            int a = 5;
+            int b = 7;
+            int max;
+            max = a > b ? a : b;
+            Console.WriteLine(max);
+            */
+
+            //string[] arr = new string[3];
+            //arr[0] = "aa";
+            //arr[1] = "bb";
+            //arr[2] = "cc";
+            //string s = Console.ReadLine();
+            
+             /*
+             //1*
+
+             for(int i = 0; i<arr.Length; i++)
+             {
+                 if(s == arr[i])
+                 {
+                     Console.WriteLine($"Index = {i}");
+                     Console.WriteLine($"Value = {arr[i]}");
+                     break;
+                 }
+             }
+             */
+
+            /*
+             //2*
+             foreach(string item in arr)
+             {
+                 if(s == item)
+                 {
+                     Console.WriteLine($"Index = {Array.IndexOf(arr, item)}");
+                     Console.WriteLine($"Value = {item}");
+                     break;
+                 }
+             }
+            */
+
+
+            //__________________________________________________________________________
+            /*
+            //Sort numbers using Bubble Sort
+
+            int[] arr = new int[6];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]=");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+
+            */
+
+            
+            //Sort numbers using one for loop       O(n)
+            
+            int[] arr = new int[6];
+            for (int i = 0; i<arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]=");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int temp;
+            for (int i = 0; i<arr.Length-1; i++)
+            {
+                    if (arr[i] > arr[i+1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i + 1] = temp;
+                        i = -1;
+                    }
+            }
+            for(int i = 0; i<arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+        
+        
+        }
+    }
+}
