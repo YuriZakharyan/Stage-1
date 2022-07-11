810698df37a4a4682a27a4a52a966c34ee4d3980
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-07-11 16:55:32 +0400

    any changes/additions

--- Arrays_Ex/Program.cs
+++ Arrays_Ex/Program.cs
@@ -703,8 +703,97 @@ namespace Arrays_Ex
             Console.WriteLine("Output: " + mid);
             */
 
+            /*
+            //451
 
 
-        }
+            Console.Write("Input m: ");
+            int m = int.Parse(Console.ReadLine());
+            int[,] arr = new int[m, m];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"arr[{i},{j}] = ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if(i+j == arr.GetLength(1)-1  || i == j)
+                    {
+                        sum += arr[i, j];
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Output: {sum/count}");
+
+            */
+
+            /*
+            //461-print row numbers which elements sorted in ascending order
+
+            Console.Write("Input m: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Input n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] arr = new int[m, n];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"arr[{i},{j}] = ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            int count = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                count = 0;
+                for (int j = 1; j < arr.GetLength(1); j++)
+                { 
+                    if(arr[i,j-1] <= arr[i, j])
+                    {
+                        count++;
+                    }                
+                }
+                if(count == arr.GetLength(1)-1)
+                {
+                    Console.WriteLine("Index " + i);
+                }
+            }
+            */
+
+
+
+
+
+
+
+
+
+
+        } 
     }
 }
