0962feaf7f97a62c55025f357db3c203352b001e
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-07-13 22:13:56 +0400

    Lesson8/Enums,Class...

---
+++ Lesson 8/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_8
+{
+    class Class1
+    {
+        public enum Day
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ Lesson 8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static Lesson_8.Class1;
+
+namespace Lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            while (true)
+            {
+                Console.Write("Num: ");
+                bool num_ = int.TryParse(Console.ReadLine(), out num);
+                if (num_ == false)
+                {
+                    Console.WriteLine("Invalid Input");
+                    return;
+                }
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine(Day.Monday);
+                        break;
+                    case 2:
+                        Console.WriteLine(Day.Tuesday);
+                        break;
+                    case 3:
+                        Console.WriteLine(Day.Wednesday);
+                        break;
+                    case 4:
+                        Console.WriteLine(Day.Thursday);
+                        break;
+                    case 5:
+                        Console.WriteLine(Day.Friday);
+                        break;
+                    case 6:
+                        Console.WriteLine(Day.Saturday);
+                        break;
+                    case 7:
+                        Console.WriteLine(Day.Sunday);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+
+        }   
+    }
+}
