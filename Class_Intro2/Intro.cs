dddb10f26402acb913c6f4f09e9a159ccf9f1082
YuriZakharyan <97274734+YuriZakharyan@users.noreply.github.com>
2022-07-16 01:41:19 +0400

    Car_Class

---
+++ Class_Intro2/Intro.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Class_Intro2
+{
+    static class Intro
+    {
+        public static string[] CarNames = new string[] { "BMW", "Mercedes", "Opel", "Opel" };
+        public static decimal[] CarPrice = new decimal[] { 30000, 30000, 25000, 10000 };
+
+        static Intro()
+        {
+        }
+
+
+        public static bool IsEqual()
+        {
+            if(CarNames.Length != CarPrice.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+        public static void PrintNames(decimal price)
+        {
+            if (IsEqual() == false)
+            {
+                Console.WriteLine("Arrays isn't equal");
+
+            }
+            else
+            {
+                int count = 0;
+                for (int i = 0; i < CarPrice.Length; i++)
+                {
+                    if (price == CarPrice[i])
+                    {
+                        count++;
+
+                        Console.WriteLine($"{i}--->" + CarNames[i].ToString());
+                    }
+                }
+                if (count == 0)
+                {
+                    Console.WriteLine($"There is no such car here, the price of which is equal to {CarPrice}");
+                }
+            }
+        }
+
+        public static void PrintValues(string name)
+        {
+            int count2 = 0;
+            if (IsEqual() == false)
+            {
+                Console.WriteLine("Arrays isn't equal");
+
+            }
+            else
+            { 
+                for(int i = 0; i<CarNames.Length; i++)
+                {
+                    if(name == CarNames[i] || name.ToLower() == CarNames[i].ToLower())
+                    {
+                        count2++;
+                        Console.WriteLine(CarPrice[i]);
+                    }
+                }
+                if(count2 == 0)
+                {
+                    Console.WriteLine($"There is no Car which name is {name}");
+                }
+
+            
+            
+            
+            
+            }
+
+            }
+
+
+    }
+}
---
+++ Class_Intro2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Class_Intro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            decimal value1, value2;
+            Console.WriteLine("---What do you want---");
+            Console.WriteLine("1. Input value of car and return their respective car names");
+            Console.WriteLine("2. Input Names of Cars and return Values");
+            bool b = int.TryParse(Console.ReadLine(), out k);
+            if (b == false)
+            {
+                Console.WriteLine("Invalid Input!");
+                return;
+            }
+            else
+            {
+                switch (k)
+                {
+                    case 1:
+                        Console.Write("Input value: ");
+                        bool t = decimal.TryParse(Console.ReadLine(), out value1);
+                        if (t == false)
+                        {
+                            Console.WriteLine("Invalid Input");
+                            return;
+                        }
+                        else
+                        {
+                            Intro.PrintNames(value1);
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Input Name of Car you want: ");
+                        string name = Console.ReadLine();
+                        Intro.PrintValues(name);
+                        break;
+                    default:
+                        Console.WriteLine("Default!!!!");
+                        break;
+
+                }
+            }
+        }
+    }
+}
